860647ea03b80214b522bc3372760e823db8929f
Daniel gtz <gutierrezdaniel822@gmail.com>
2019-11-05 19:00:09 -0600

    PrimerVersión

---
+++ src/Inicio.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class Inicio extends JFrame {
+    public static String[] preguntas= new String[10];
+    private static int i=0;
+    private  static  int time=6;
+
+
+    public Inicio(){
+        super("Historia de Mexico");
+        preguntas[0]="¿quien fue el primer presidente de méxico?";
+        preguntas[1]="¿quien descubrio america?";
+        preguntas[2]="¿quien inicio la revolución?";
+        preguntas[3]="¿Deidad del México antiguo conocida como la “Serpiente Emplumada”?";
+        preguntas[4]="¿Cultura que se desarrolló en la ciudad de Palenque?";
+        preguntas[5]="¿Las cabezas colosales son creaciones de la cultura?";
+        preguntas[6]="¿En dónde se encuentran las pirámides del Sol y la Luna?";
+        preguntas[7]="¿La base alimenticia de los pueblos mesoamericanos fue?";
+        preguntas[8]="¿ Chichén Itzá fue una poderosa ciudad?";
+        preguntas[9]="¿ Año en que los españoles conquistaron México Tenochtitlán?";
+        setSize(1200,650);
+        setLocation(50, 50);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+
+        //contenedor
+        Container cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+        /////////////////////////////////botones
+
+        JButton bPreguntas= new JButton("Buscar pregunta");
+        bPreguntas.setBounds(450,200,150,30);
+        bPreguntas.setBackground(Color.white);
+
+        JButton btnFalse= new JButton("FALSE");
+        btnFalse.setBounds(50,30,150,30);
+        btnFalse.setBackground(Color.white);
+
+        JButton btnTrue= new JButton("TRUE.");
+        btnTrue.setBounds(850,30,150,30);
+        btnTrue.setBackground(Color.white);
+
+        /////
+        Label labelPreguntas= new Label("preguntas");
+        labelPreguntas.setBounds(250,100,1000,55);
+        labelPreguntas.setFont(new Font("Verdana", Font.PLAIN, 25));
+        labelPreguntas.setPreferredSize(new Dimension(250, 250));
+        ///////////////////////////////////////////////////////////////////////////////77
+        Label labelTiempo= new Label("00");
+        labelTiempo.setBounds(450,280,500,55);
+        labelTiempo.setFont(new Font("Verdana", Font.PLAIN, 25));
+        labelTiempo.setPreferredSize(new Dimension(250, 250));
+        //////////////////////////////////////////////////////////////////////////////////////////7
+        bPreguntas.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                time=6;
+                int num= numAleatorio();
+                String pregunta= preguntas[num];
+                labelPreguntas.setText(pregunta);
+                while(0<time) {
+                    labelTiempo.setText(time + "s Restantes");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        ex.printStackTrace();
+                    }
+                    time--;
+
+                }
+            }
+        });
+
+
+
+        setLayout(null);
+        cp.add(labelPreguntas);
+        cp.add(bPreguntas);
+        cp.add(labelTiempo);
+        cp.add(btnFalse);
+        cp.add(btnTrue);
+
+
+
+
+    }
+    public static int numAleatorio(){
+        //numeroo aleatorio
+        Random aleatorio = new Random(System.currentTimeMillis());
+        int intAletorio = aleatorio.nextInt(9);
+        return intAletorio;
+
+    }
+
+
+}
---
+++ src/VentanaJuego.java
@@ -0,0 +1,57 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VentanaJuego extends JFrame {
+    public VentanaJuego(){
+        super("Juego Historia");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(1200,650);
+        setLocation(50, 50);
+        ///dibujar el contenedor
+        Container frame= getContentPane();
+        frame.setLayout(new FlowLayout());
+
+        Button bEntrar = new Button("Entrar al juego");
+        bEntrar.setBounds(550, 500, 140, 30);
+        bEntrar.setBackground(Color.GRAY);
+        JLabel textoBienvenida= new JLabel("Bienvenido");
+        textoBienvenida.setBackground(Color.blue);
+        textoBienvenida.setBounds(550, 10, 100, 20);
+        ImageIcon img= new ImageIcon("D:\\Descargas\\UV-Agosto19-Enero20\\uv-7\\interfaces de usuario avanzada\\ProyectoHistoria\\img\\historia.jpg");
+        JLabel labelImage= new JLabel(img);
+        labelImage.setBounds(300, 150, 300, 300);
+
+        //codigo del boton
+        bEntrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Inicio nueva=  new Inicio();
+                setVisible(false);
+            }
+        });
+
+
+
+        setLayout(null);
+        frame.add(bEntrar);
+        frame.add(labelImage);
+        frame.add(textoBienvenida);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
